24dcb6ea2a8b82fbe923853ded612874a82ff5a1
Thitari-Somboon <thitari.somboon@students.fhnw.ch>
2020-11-13 14:32:47 +0100

    Customer Sign up page edited

---
+++ design/assets/js/js-Sign-Up/Sign-up.js
@@ -0,0 +1,24 @@
+/*
+ * This is function to repose information required 
+ * at Sign-up page
+ */
+
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
