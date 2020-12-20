1a39ce13d5f7c5047ae3956647a56763f59384f1
Thitari-Somboon <thitari.somboon@students.fhnw.ch>
2020-12-20 20:48:55 +0100

    Create Admin dashboard Admin Customerlist Admin Reservation

---
+++ design/assets/js/js-admin/initiate-datatables.js
@@ -0,0 +1,8 @@
+// Initiate datatables in roles, tables, users page
+$('#dataTables-reservation').DataTable({
+    responsive: true,
+    pageLength: 20,
+    lengthChange: false,
+    searching: true,
+    ordering: true
+});
