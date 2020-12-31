037e4bfdc0dac6c3fcdfa3ccb86ddf3efa76b562
Thitari-Somboon <thitari.somboon@students.fhnw.ch>
2020-12-31 18:51:40 +0100

    Cancel btn added in Cus-editprofile page Alert box for profile changed added

--- bbb/src/main/resources/static/assets/js/js-customer/cus-editProfile.js
+++ bbb/src/main/resources/static/assets/js/js-customer/cus-editProfile.js
@@ -59,3 +59,35 @@ function Validate() {
     }
     return true;
 }
+
+
+
+var alert_items = document.querySelectorAll(".alert_item");
+var btns = document.querySelectorAll(".btn_saveChange");
+var alert_wrapper = document.querySelector(".alert_wrapper");
+var close_btns = document.querySelectorAll(".close");
+
+btns.forEach(function(btn, btn_index){
+    btn.addEventListener("click", function(){
+        alert_wrapper.classList.add("active");
+
+        alert_items.forEach(function(alert_item, alert_index){
+            if(btn_index == alert_index){
+                alert_item.style.top = "50%";
+            }
+            else{
+                alert_item.style.top = "-100%";
+            }
+        })
+    })
+})
+
+close_btns.forEach(function(close, close_index){
+    close.addEventListener("click", function(){
+        alert_wrapper.classList.remove("active");
+
+        alert_items.forEach(function(alert_item, alert_index){
+            alert_item.style.top = "-100%";
+        })
+    })
+})
