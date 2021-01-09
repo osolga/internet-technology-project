55099b2a84ffec9529cb31d1d60d9672422045d1
Johaido <jonas.hainmueller@students.fhnw.ch>
2021-01-09 15:14:15 +0100

    Changes to Service Endpoint and controller

--- bbb/src/main/java/beerbellybar/process/bbb/data/repository/BarOrderRepository.java
+++ bbb/src/main/java/beerbellybar/process/bbb/data/repository/BarOrderRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<BarOrder, Long> {
+public interface BarOrderRepository extends JpaRepository<BarOrder, Long> {
 }
